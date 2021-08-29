7839101b0db938339383e64cdb2a56f5b7907453
박혜원 <79081836+phw-plain@users.noreply.github.com>
2021-08-29 14:06:12 +0900

    0829

---
+++ BACKJOON/수학/10872.py
@@ -0,0 +1,7 @@
+# 팩토리얼 문제
+N = int(input())
+sum = 1
+while N!=0:
+    sum*=N
+    N-=1
+print(sum) 
---
+++ BACKJOON/수학/10951.py
@@ -0,0 +1,7 @@
+# A+B - 4 문제
+while 1:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
